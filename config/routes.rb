d0165e72652150cd0b539194cf9aad49baedf2ff
kilimandzsaro <natalia.bakos@inbank.ee>
2017-05-19 09:42:52 +0300

    Add devise configuration and model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users, :controllert => { :passwords => "users/passwords", :confirmations => "users/confirmations" }
+  devise_scope :users do
+    get 'signup', to: "devise/registrations#new"
+    get 'signin', to: "devise/sessions#new"
+    delete 'signout', to: "devise/sessions#destroy"
+  end
   resources :businesses
   resources :componenets
   get 'static_pages/employee'
