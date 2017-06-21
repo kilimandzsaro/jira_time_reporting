5a4c8d1030c6dd925e4123a1854071198c2c2c16
kilimandzsaro <natalia.bakos@inbank.ee>
2017-06-21 08:40:29 +0300

    Add tests for components and refactor the components controller according to the test results

--- test/controllers/components_controller_test.rb
+++ test/controllers/components_controller_test.rb
@@ -12,9 +12,24 @@ class ComponentsControllerTest < ActionDispatch::IntegrationTest
 
   test "should create component" do
     assert_difference('Component.count') do
-      post components_url, params: { component: { name: @component.name } }
+      post components_url, params: { component: { name: "Random component" } }
+    end
+    assert_equal "Random component", Component.last.name
+  end
+
+  test "should get all components and upload the new ones"
+    mock = Minitest::Mock.new
+    project = Project.first
+
+    mock.expect :new, true # Mocking GetJiraResponseService.new
+    mock.expect :all, [project] # Mocking Project.all
+    mock.expect :project_components, ['another'], [project] # Mocking GetJiraResponseService.project_components(project)
+
+    GetJiraResponseService.stub :new, mock do
+      # assert_difference('Component.count') do
+        Component.get
+      # end
     end
 
-    assert_redirected_to components_url(Component.last)
-  end
+    assert_mock mock
 end
