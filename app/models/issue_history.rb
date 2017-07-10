7365e49455d9e094f03df1582d16de41d56e3651
kilimandzsaro <natalia.bakos@inbank.ee>
2017-07-10 09:43:11 +0300

    Add issue show view by issue_history

--- app/models/issue_history.rb
+++ app/models/issue_history.rb
@@ -3,4 +3,21 @@ class IssueHistory < ApplicationRecord
   belongs_to :issue
   belongs_to :status
 
+  def get_days
+    days = self.duration / 24
+    return days.to_i
+  end
+
+  def get_hours
+    hours = self.duration % 24
+    return hours.to_i
+  end
+
+  def get_minutes
+    minutes = 0.0
+    # avoid devision by 0
+    minutes = (self.duration % self.duration.to_i) * 60 if self.duration > 1
+    return minutes.round
+  end
+
 end
